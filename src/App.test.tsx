94e4620161127fd302c2a88795d9e0157f0592f7
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2024-04-01 21:56:40 +1300

    [#6] Test the form's validations

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,15 @@
 import { describe, it, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import App from "./App";
 
 beforeEach(() => {
   render(<App />);
-})
+});
 
 describe("App", () => {
-  it("has 'Sign-Up' in the heading when loaded", () => {
+  it("has 'Sign Up' in the heading when loaded", () => {
     expect(screen.getByRole("heading")).toHaveTextContent("Sign Up");
   });
 
@@ -22,7 +23,7 @@ describe("Form", () => {
 
   beforeEach(() => {
     form = screen.getByRole("form");
-  })
+  });
 
   it("has a 'Username' input", () => {
     const usernameInput = screen.getByLabelText("Username");
@@ -50,5 +51,93 @@ describe("Form", () => {
 
     expect(button).toBeInTheDocument();
     expect(form.contains(button)).toBeTruthy();
+  });
+});
+
+describe("Form's validation", () => {
+  let userNameInput: HTMLElement;
+  let createPasswordInput: HTMLElement;
+  let confirmPasswordInput: HTMLElement;
+  let signUpButton: HTMLElement;
+
+  const validEmail = "yuri@example.com";
+  const invalidEmail = "yuri@example";
+  const validPassword = "1Q!"
+  const invalidPasswordNoSpecial = "1Q";
+  const invalidPasswordNoNumeric = "Q!";
+  const invalidPasswordNoCapital = "1!";
+
+  const errorMessageInvalidEmail = "Please enter a valid email address";
+  const errorMessageInvalidPassword = "Must contain a capital letter, a digit, and a special character";
+  const errorMessagePasswordsDontMatch = "Passwords do not match";
+
+  beforeEach(() => {
+    userNameInput = screen.getByLabelText("Username");
+    createPasswordInput = screen.getByLabelText("Create password");
+    confirmPasswordInput = screen.getByLabelText("Confirm password");
+    signUpButton = screen.getByRole("button", { name: "Sign Up" });
+  });
+
+  it("displays an error message when the username input is empty", async () => {
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessageInvalidEmail)
+    ).toBeInTheDocument();
+  });
+
+
+  it("displays an error message when the username input is not an email", async () => {
+    await userEvent.type(userNameInput, invalidEmail);
+    await userEvent.type(createPasswordInput, validPassword);
+    await userEvent.type(confirmPasswordInput, validPassword);
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessageInvalidEmail)
+    ).toBeInTheDocument();
+  });
+
+  it("displays an error message when the password input is empty", async () => {
+    await userEvent.type(userNameInput, validEmail);
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessageInvalidPassword)
+    ).toBeInTheDocument();
+  });
+
+  it("displays an error message when the password lacks a special symbol", async () => {
+    await userEvent.type(userNameInput, validEmail);
+    await userEvent.type(createPasswordInput, invalidPasswordNoSpecial);
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessageInvalidPassword)
+    ).toBeInTheDocument();
+  });
+
+  it("displays an error message when the password lacks a numeric symbol", async () => {
+    await userEvent.type(userNameInput, validEmail);
+    await userEvent.type(createPasswordInput, invalidPasswordNoNumeric);
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessageInvalidPassword)
+    ).toBeInTheDocument();
+  });
+
+  it("displays an error message when the password lacks a capital letter", async () => {
+    await userEvent.type(userNameInput, validEmail);
+    await userEvent.type(createPasswordInput, invalidPasswordNoCapital);
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessageInvalidPassword)
+    ).toBeInTheDocument();
+  });
+
+  it("displays an error message when the passwords do not match", async () => {
+    await userEvent.type(userNameInput, validEmail);
+    await userEvent.type(createPasswordInput, validPassword);
+    await userEvent.type(confirmPasswordInput, invalidPasswordNoSpecial); 
+    await userEvent.click(signUpButton);
+    expect(
+      await screen.findByText(errorMessagePasswordsDontMatch)
+    ).toBeInTheDocument();
   });
 });
